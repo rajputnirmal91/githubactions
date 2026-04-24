1f5c234ee5d93509f6a8073134f8afbccface482
rajputnirmal91 <rajputnirmal91@gmail.com>
2026-04-24 10:10:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+export default function UserList(props) {
+const [users, setUsers] = useState([]);
+const [filter, setFilter] = useState("");
+
+
+useEffect(() => {
+fetch("/api/users")
+.then((res) => res.json())
+.then((data) => {
+setUsers(data);
+});
+}, []);
+
+
+const filteredUsers = users.filter((u) => {
+return u.name.toLowerCase().includes(filter.toLowerCase());
+});
+
+const handleClick = (user) => {
+console.log("User clicked", user);
+};
+
+return ( <div> <h2>User List</h2>
+
+  
+  <input
+    value={filter}
+    onChange={(e) => setFilter(e.target.value)}
+    placeholder="Search user"
+  />
+
+  
+  <ul>
+    {filteredUsers.map((user, index) => (
+    
+      <li key={index} onClick={() => handleClick(user)}>
+     
+        {user.name.toUpperCase()}
+
+     
+        <div
+          dangerouslySetInnerHTML={{ __html: user.bio }}
+        ></div>
+      </li>
+    ))}
+  </ul>
+
+ 
+  {props.showCount && <p>Total: {users.length}</p>}
+</div>
+```
+
+);
+}
